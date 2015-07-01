0a5214525fa056b564c9bd800206461ccf97dca7
zyp <15032107509@163.com>
2015-07-01 04:02:33 -0700

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage) 	modified:   mongodb/read_mongo_test.py    (use "git add <file>..." to include in what will be committed) 	mongodb/call_shell_exe_mongo.sh 	mongodb/read_mongo_test.py.bak 	mongodb/statistic_dicts_maps.py 	mongodb/tools_python.py

--- bj/mongodb/read_mongo_test.py
+++ bj/mongodb/read_mongo_test.py
@@ -3,50 +3,137 @@
 
 import hashlib
 import pymongo
-def main():
-    SOURCE_IP = "10.136.20.106"
-    SOURCE_PORT = 27070
+
+MAX_SUBTPIC = 2000
+account_openid_weight = {}
+
+def garry_statistics():
+    #openid, account, topic1, sutopic_1, total_num, topic_num , subtopic_num  对每一个topic1, 按照 subtopic_num排序
+    print "in garry"
+    SOURCE_IP = "10.144.85.109"
+    SOURCE_PORT = 17016
     conn_src = pymongo.Connection(host=SOURCE_IP,port=SOURCE_PORT)
-    db_src_article = conn_src.WeiXinRecom.weixin_articles
-    res_arts = db_src_article.find({'keywords':{'$exists':True}},{'keywords':1}).limit(30)#pymongo is sensitive '',True not true
+    #db_src_article = conn_src.WeiXinRecom.weixin_articles# sorurce DB
+    db_src_article = conn_src.WeiXinRecom.weixin_articles # sorurce DB
+    res_arts = db_src_article.find({},{"topic1":1,"subtopic_1":1,"account_openid":1,"account":1})#.limit(10000)#pymongo is sensitive '',True not true
+    topic1_account_openid_subtopic_1_num = {}
+    account_openid_num = {}
     
-    cp = 0
+    cnt_topic1 = 0
+    cnt_subtopic_1 = 0
+    # read DBs
     for art in res_arts:
-        cp = cp + 1
-        print "di:%d -- art:id = %s ;" %(cp,art.get('_id','10000').encode('gbk','ignore'))
-        keywords = art.get('keywords',[])
-        if not keywords:# empty
-            print "keywords is an empty list[]..."
-            continue
-        for word in keywords:#keywords is collection of lists
-            if not word:
-                print "kong"
-            for ele in word:
-                if type(ele)==list:
-                    for e in ele:
-                        print "e = %s" %e
-                else:#word,pos
-                    print "ele = %s" % ele#.encode('gbk','ignore')
-    # close
+        topic1 = art.get('topic1',"kong top")
+        subtopic_1 = art.get('subtopic_1','kong sub')
+        account_openid = art.get('account_openid','kong acc_op')
+        account = art.get('account','kong acc')
+        account_openid += ("\t" + account)
+        if not topic1:
+            topic1 = "others"
+            cnt_topic1 += 1
+        if not subtopic_1:
+            subtopic_1 = "sub_others"
+            cnt_subtopic_1 += 1
+        # statistic
+        if topic1 not in topic1_account_openid_subtopic_1_num:
+            topic1_account_openid_subtopic_1_num[topic1] = {}
+            if account_openid not in topic1_account_openid_subtopic_1_num[topic1]:
+                topic1_account_openid_subtopic_1_num[topic1][account_openid] = {}
+                if subtopic_1 not in topic1_account_openid_subtopic_1_num[topic1][account_openid]:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] = 1
+                else:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] += 1
+            else:
+                if subtopic_1 not in topic1_account_openid_subtopic_1_num[topic1][account_openid]:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] = 1
+                else:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] += 1
+               
+        else:
+            if account_openid not in topic1_account_openid_subtopic_1_num[topic1]:
+                topic1_account_openid_subtopic_1_num[topic1][account_openid] = {}
+                if subtopic_1 not in topic1_account_openid_subtopic_1_num[topic1][account_openid]:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] = 1
+                else:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] += 1
+            else:
+                if subtopic_1 not in topic1_account_openid_subtopic_1_num[topic1][account_openid]:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] = 1
+                else:
+                    topic1_account_openid_subtopic_1_num[topic1][account_openid][subtopic_1] += 1
+
+        # statistic by account_openid
+        account_openid_num[account_openid] = account_openid_num.get(account_openid,0) + 1
+    # end for
     conn_src.close()
-    '''格式如下：
-    { "_id" : "42c4a9be657885b3df63e47d4d41308f", "keywords" : [ ] }
-    { "_id" : "689ef41d47a05c5159e06e04b4c0165e", "keywords" : [  [  "锟斤拷锟斤拷",  [  1,  "锟斤拷司锟斤拷锟斤拷" ] ],  [  "锟斤拷锟斤拷",  [  1,  "锟斤拷锟斤拷锟斤拷锟斤拷" ] ] ] }
-    { "_id" : "76edf0b9b3c246eec6c1ab4b0e54e234", "keywords" : [  [  "装锟斤拷",  5 ],  [  "锟斤拷锟斤拷",  5 ],  [  "装锟斤拷",  5 ] ] }
+    # write the rusults
+    fw = open("statistics_0630new.data","w")
+    print >> fw, "account_openid\taccount\ttopic1\tsubtopic_1\ttotal_num\ttopic1_value\tsubtopic_1_value"
+    for topic1, topic1_value in topic1_account_openid_subtopic_1_num.items():
+        subtopic_1_value_temp = {} # rebuild the key value
+        for account_openid, account_openid_value in topic1_value.items():
+            topic1_value = sum(account_openid_value.values())
+            for subtopic_1, subtopic_1_value in account_openid_value.items():
+                key = "%s\t%s\t%s\t%d\t%d" % \
+                    (account_openid,topic1,subtopic_1,account_openid_num[account_openid],\
+                    topic1_value)
+                subtopic_1_value_temp[key] = subtopic_1_value#end for subtopic_1 and account_openid
+        subtopic_1_value_sort = sorted(subtopic_1_value_temp.items(), key=lambda d:d[1], reverse=True)
 
-    isinstance可以用来判断一个变量是否属于一个类。 在python里应该是正确的。
-     if type(x)==list:pass
-      if type(x)==dict:pass
-    '''
+        subtopic_1_cnt = 0
+        for key, key_value in subtopic_1_value_sort:
+            #subtopic_1_cnt += 1
+            if subtopic_1_cnt >= MAX_SUBTPIC:
+                break
+            #account_openid = key.strip().split('\t',1)[0]
+            # original DBs
+            key_eles = key.strip().split('\t')
+            account_openid= key_eles[0]
+            topic1 = key_eles[2]
+            sutopic_1 = key_eles[3]
+            #print account_openid,topic1,sutopic_1
+            # from file of ./account_weight.mysql
+            account_openid_weight_values = account_openid_weight.get(account_openid)
+            if not account_openid_weight_values:# bu qi
+                account_openid_weight_values = "\t0\t\t0"
+            # from file
+            account_openid_weight_values_eles = account_openid_weight_values.split('\t')
+            topic1_file = account_openid_weight_values_eles[0]
+            sutopic_1_file = account_openid_weight_values_eles[2]
+            #print topic1_file,sutopic_1_file
+            if not topic1_file:
+                subtopic_1_cnt += 1
+                print >> fw, ("%s\t%d\t%s") % (key, key_value, account_openid_weight_values)
+            elif topic1_file == topic1:
+                if not sutopic_1_file:
+                    subtopic_1_cnt += 1
+                    print >> fw, ("%s\t%d\t%s") % (key, key_value, account_openid_weight_values)
+                elif sutopic_1_file == sutopic_1:
+                    subtopic_1_cnt += 1
+                    print >> fw, ("%s\t%d\t%s") % (key, key_value, account_openid_weight_values)
+                else:
+                    pass
 
-def split_join():
-    str1 = '1,2,3'
-    str_list = str1.split(',',1)
-    print str_list
+            else:
+                pass
+                
+                
+            
+            #print account_openid
+            #print >> fw, ("%s\t%d\t%s") % (key, key_value, account_openid_weight_values)
 
-    str_2 = '\t'.join(str_list)
-    print str_2
+    # end for
+    fw.close()
+
+def init_load_account_weight():
+    print "BEGIN init_load_account_weight..."
+    with open("./account_weight.mysql",'r') as fr:
+        for line in fr:
+            fields = line.strip().split("\t",1)
+            account_openid_weight[fields[0]] = fields[1]
+    print "END init_load_account_weight..."
+
 
 if (__name__ == "__main__"):
-    main()
-    split_join()
+    init_load_account_weight()
+    garry_statistics()
